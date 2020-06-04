3c4f8fbe266ff3a9ce3f05b51f91afc6b10277ae
Fatih Melih Ersoy <fatihmelih9@hotmail.com>
2020-06-04 23:38:20 +0300

    kullanım talimatları eklendi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SAU_BOT
  
+YOUR_CHANNEL_ID, YOUR-MESSAGE-ID ve YOUR_TOKEN kısımlarını değiştirerek kullanabilirsiniz.
+
+Daha ayrıntılı bilgi kodun içinde yorum satırı olarak yazıldı.
--- bot.py
+++ bot.py
@@ -1,14 +1,26 @@
+#gerekli modüller
 import discord
 from discord.ext    import commands
 from discord.ext.commands   import Bot
 import asyncio
 
+
+
+# botun çağırılacağı ön ek
 bot = commands.Bot(command_prefix = 'sau')
 
+
+
+# botun çalıştığını konsola 'bot is ready' yazısının çıkmasıyla anlayabilirsiniz
 @bot.event
 async def on_ready():
     print('bot is ready')
 
+
+
+
+# Kullanıcı sunucuya katıldığında belirlenen ID'ye sahip kanala 
+# kullanıcının ismini ve toplam üye sayısını yazar
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(YOUR_CHANNEL_ID)
@@ -16,8 +28,12 @@ async def on_member_join(member):
     guild = member.guild
     lst = len(list(guild.members))
 
-    await channel.send(f'{member.mention} sunucuya katıldı 🤙 Üye sayısı: {lst}')
+    await channel.send(f'{member.mention} sunucuya katıldı 🤙 Üye sayısı: {lst+1}')
 
+
+    
+# Kullanıcı sunucudan ayrıldığında belirlenen ID'ye sahip kanala 
+# kullanıcının ismini ve toplam üye sayısını yazar
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(YOUR_CHANNEL_ID)
@@ -25,10 +41,12 @@ async def on_member_remove(member):
     guild = member.guild
     lst = len(list(guild.members))
 
-    await channel.send(f'{member.mention} sunucudan ayrıldı 👋 Üye sayısı: {lst+2}')
+    await channel.send(f'{member.mention} sunucudan ayrıldı 👋 Üye sayısı: {lst+1}')
     
 
-
+    
+    
+# Belirlenen kanal ID'sindeki mesajların tümüne tepki uygular (altına buton ekler)
 @bot.event
 async def on_message(message):
     if message.channel.id == YOUR_CHANNEL_ID:
@@ -38,6 +56,9 @@ async def on_message(message):
         await message.add_reaction("🇩")
         await message.add_reaction("🇪")
 
+        
+        
+# mesaj silme örn chat'e 'sausil 50' yazıldığında 50 mesaj silinir 
     if message.content.startswith('sausil'):
         if message.author.permissions_in(message.channel).manage_messages:
             args = message.content.split(' ')
@@ -48,16 +69,18 @@ async def on_message(message):
                     await message.channel.send('{} mesaj silindi'.format(len(deleted)-1))
 
 
+
+# belirlenen mesaj id'sinde belirlenen bir emojiye basıldığında belirtilen rol verilir. bkz. satır 69-73-75
 @bot.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
-    if message_id == YOUR_MESSAGE_ID:
+    if message_id == YOUR_MESSAGE_ID: #MESAJ ID
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
 
-        if payload.emoji.name == 'sau':
+        if payload.emoji.name == 'sau': #EMOJI ISMI
             print('sau role ok')
-            role = discord.utils.get(guild.roles, name='Member')
+            role = discord.utils.get(guild.roles, name='Member') #ROL ISMI
 
         if role is not None:
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
@@ -65,16 +88,18 @@ async def on_raw_reaction_add(payload):
                 await member.add_roles(role)
                 print('add done')
 
+                
+# belirlenen mesaj id'sinde belirlenen emojiye basılı olduğu halde tekrar basma durumunda belirtilen rol kaldırılır. bkz. satır 88-92-94
 @bot.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
-    if message_id == YOUR_MESSAGE_ID:
+    if message_id == YOUR_MESSAGE_ID: #MESAJ ID
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
 
-        if payload.emoji.name == 'sau':
+        if payload.emoji.name == 'sau': #EMOJI ISMI
             print('sau role deleted')
-            role = discord.utils.get(guild.roles, name='Member')
+            role = discord.utils.get(guild.roles, name='Member') #ROL ISMI
 
         if role is not None:
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
@@ -82,4 +107,8 @@ async def on_raw_reaction_remove(payload):
                 await member.remove_roles(role)
                 print('delete done')
 
+                
+                
+                
+# Son olarak bot tokeninizi girip botu çalıştırabilirsiniz
 bot.run('YOUR_TOKEN')
